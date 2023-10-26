b30ecb96e6ca3d0e6c70d4311869508374f2d22e
iijima-naoya-45b <meteor5963@gmail.com>
2023-10-26 22:49:10 +0900

    carrierwaveの設定

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [80, 80]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
