ead69da7d450c50449e908fc37f0f4b068dd5298
iijima-naoya-45b <meteor5963@gmail.com>
2023-10-26 11:55:45 +0900

    改修

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'https://qmeals-app.onrender.com',
+  user_name:            'qmeals',
+  password:             'qmeals',
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
