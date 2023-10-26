dd7c2c8aabe98e9ed54b89dcd5fa68966fed99c5
iijima-naoya-45b <meteor5963@gmail.com>
2023-10-26 21:37:33 +0900

    回答しました

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,6 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_comments, dependent: :destroy
   has_many :recipe_photos, dependent: :destroy
-  has_many :recipe_ratings, dependent: :destroy
   has_many :recipe_type_of_tags, dependent: :destroy
   has_many :recipe_tags, through: :recipe_type_of_tags
   has_many :favorite_recipes, dependent: :destroy
