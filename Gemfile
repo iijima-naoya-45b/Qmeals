ba2044016b3f0f1b66c459bc624b41d4924c4114
iijima-naoya-45b <meteor5963@gmail.com>
2023-10-27 15:51:41 +0900

    画像の圧縮をしました

--- Gemfile
+++ Gemfile
@@ -87,6 +87,8 @@ gem 'fog-aws'
 
 gem 'aws-sdk-s3', require: false
 
+gem "piet"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'capybara'
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -31,7 +31,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_limit: [300, 300]
+  process resize_to_fill: [500, 500]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
@@ -42,7 +42,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    original_filename if original_filename
-  end
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
 end
