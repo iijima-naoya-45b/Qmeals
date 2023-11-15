544e9e9a7225b3e1bf1a1f552842a30e38dae699
iijima-naoya-45b <meteor5963@gmail.com>
2023-11-15 23:04:41 +0900

    line認証テスト

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,28 +1,36 @@
 class OauthsController < ApplicationController
-  skip_before_action :require_login, raise: false
+  skip_before_action :require_login
 
   def oauth
-    login_at(params[:provider])
+    login_at(auth_params[:provider])
   end
 
   def callback
-    provider = params[:provider]
-    begin
-      if @user = login_from(provider)
+    provider = auth_params[:provider]
+    if (@user = login_from(provider))
+      flash[:success] = "#{provider.titleize}アカウントでログインしました"
+      redirect_to root_path
+    else
+      begin
+        signup_and_login(provider)
+        flash[:success] = "#{provider.titleize}アカウントでログインしました"
         redirect_to root_path
-        flash[:success] = "#{provider.titleize}アカウントでログインしました。"
-      else
-        begin
-          @user = create_from(provider)
-          reset_session
-          auto_login(@user)
-          redirect_to root_path
-          flash[:success] = "#{provider.titleize}アカウントでログインしました。"
-        rescue
-          redirect_to login_path
-          flash[:danger] = "#{provider.titleize}アカウントでのログインに失敗しました。"
-        end
+      rescue
+        flash[:danger] = "#{provider.titleize}アカウントでのログインに失敗しました"
+        redirect_to root_path
       end
     end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider, :error, :state)
+  end
+
+  def signup_and_login(provider)
+    @user = create_from(provider)
+    reset_session
+    auto_login(@user)
   end
 end
